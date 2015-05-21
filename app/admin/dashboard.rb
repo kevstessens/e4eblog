aba911e02a446ba3ab224c320c9dfac80b4677f2
Kevin Stessens <kevin@tekgenesis.com>
2015-05-21 11:04:12 -0300

    active skin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,12 +1,8 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page "Dashboard" do #
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-
-
-    Here is an example of a simple dashboard with columns and panels.
     
-    columns do
+    columns do#
       column do
         panel "Últimos posts" do
           ul do
@@ -17,12 +13,11 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
 
-      column do
-        panel "Información general" do
+      column do#
+        panel "Información general" do#
           para "Bienvenido al panel de administración de E4E. Desde acá podés administrar los posts, categorías, autores y mucho más que está por venir."
           para "Si necesitás ayuda, contactate con kevin@e4e.la, jgesino@e4e.la o paca@e4e.la"
         end
-      end
-    end
-  end # content
-end
+      end#
+    end#
+end #
